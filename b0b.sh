5e9de3f6ef62c6b1547a2539030c8afc385a09e2
Cliffano Subagio <cliffano@gmail.com>
2010-12-07 20:51:49 +1100

    Removed TOOL_DIR.

--- b0b.sh
+++ b0b.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 
-TOOL_DIR="/var/www/tool"
-
-make -f $TOOL_DIR/b0b/Makefile \
+make -f /var/www/tool/b0b/Makefile \
     APP_NAME="b0b" \
     APP_VERSION="0.1-SNAPSHOT" \
     APP_DIR=`pwd` \
     DEPLOY_SUBDIR="tool/b0b" \
-    TOOL_DIR="$TOOL_DIR" \
     $1 $2 $3 $4 $5 $6 $7 $8 $9
