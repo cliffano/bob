0903db5076aa5e7dbcdc252c46e931c0cf7764d8
Cliffano Subagio <cliffano@gmail.com>
2011-06-26 11:43:00 +1000

    Fixed custom source dir and deploy config handling.

--- bin/package.py
+++ bin/package.py
@@ -4,7 +4,13 @@ import os
 file = open('./package.json')
 conf = json.load(file)
 out = 'APP_NAME="' + conf['name'] + '"\n' + 'APP_VERSION="' + conf['version'] + '"\n'
-if hasattr(conf, 'app') and hasattr(conf['app'], 'deploy'):
+try:
+    out += 'APP_SRC_DIR="' + conf['app']['src']['dir'] + '"\n'
+except:
+    print('# Using default source dir')
+try:
     out += 'DEPLOY_HOST="' + conf['app']['deploy']['host'] + '"\n' + 'DEPLOY_PORT="' + conf['app']['deploy']['port'] + '"\n' + 'DEPLOY_DIR="' + conf['app']['deploy']['dir'] + '"\n'
+except:
+    print('# No deploy info')
 print(out)
 file.close()
