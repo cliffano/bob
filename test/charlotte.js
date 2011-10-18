57cf955a4f69aebb6b70bab75b560ab93ede0c4f
Cliffano Subagio <cliffano@gmail.com>
2011-10-18 22:47:39 +1100

    Set version to 0.0.1 when version is undefined.

--- test/charlotte.js
+++ test/charlotte.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
     sandboxedmodule = require('sandboxed-module'),
     vows = require('vows');
 
-function getVersionUpCharlotte(origVersion, newVersion) {
+function getVersxionUpCharlotte(origVersion, newVersion) {
     var mockFs = {
             readFileSync: function (file) {
                 assert.equal(file, 'package.json');
@@ -22,27 +22,36 @@ function getVersionUpCharlotte(origVersion, newVersion) {
 
 vows.describe('Charlotte').addBatch({
     'versionup': {
-        topic: function () {
-            return {
-                readFileSync: function (file) {
-                    assert.equal(file, 'package.json');
-                    return '{ "version": "0.0.0" }';
+        topic: function getVersionUpCharlotte(origVersion, newVersion) {
+            var mockFs = {
+                    readFileSync: function (file) {
+                        assert.equal(file, 'package.json');
+                        return (origVersion === undefined) ? '{}' : '{ "version": "' + origVersion + '" }';
+                    },
+                    writeFileSync: function (file, data) {
+                        assert.equal(file, 'package.json');
+                        assert.equal(JSON.parse(data).version, newVersion);
+                    }
+                };
+            return sandboxedmodule.require('../lib/charlotte', {
+                requires: {
+                    'fs': mockFs    
                 }
-            };
+            });
         },
-        'versionup should upgrade version build number': function (mockFs) {
+        'versionup should upgrade version build number': function (getVersionUpCharlotte) {
             var charlotte = getVersionUpCharlotte('0.0.0', '0.0.1');
             assert.equal((new charlotte.Charlotte()).versionup(), '0.0.1');
         },
-        'versionup minor should upgrade version minor number': function (mockFs) {
+        'versionup minor should upgrade version minor number': function (getVersionUpCharlotte) {
             var charlotte = getVersionUpCharlotte('0.0.0', '0.1.0');
             assert.equal((new charlotte.Charlotte()).versionup('minor'), '0.1.0');
         },
-        'versionup major should upgrade version major number': function (mockFs) {
+        'versionup major should upgrade version major number': function (getVersionUpCharlotte) {
             var charlotte = getVersionUpCharlotte('0.0.0', '1.0.0');
             assert.equal((new charlotte.Charlotte()).versionup('major'), '1.0.0');
         },
-        'versionup should set version to 0.0.1 when original version is undefined': function (mockFs) {
+        'versionup should set version to 0.0.1 when original version is undefined': function (getVersionUpCharlotte) {
             var charlotte = getVersionUpCharlotte(undefined, '0.0.1');
             assert.equal((new charlotte.Charlotte()).versionup(), '0.0.1');
         },
