81d42fd9faf96c0190dbc8ce39d444114df4f5ee
Cliffano Subagio <cliffano@gmail.com>
2012-02-10 18:03:16 +1100

    Clean up lint violations.

--- test/util.js
+++ test/util.js
@@ -17,38 +17,38 @@ vows.describe('util').addBatch({
     },
     'when data is null': {
       'then value should be the original data': function (topic) {
-        var val = topic.apply(null, { foo: 'bar' });
-        assert.isNull(val);
+        var value = topic.apply(null, { foo: 'bar' });
+        assert.isNull(value);
       }
     },
     'when params is null': {
       'then value should be the original data': function (topic) {
-        var val = topic.apply('hello ${foo} world', null);
-        assert.equal(val, 'hello ${foo} world');
+        var value = topic.apply('hello ${foo} world', null);
+        assert.equal(value, 'hello ${foo} world');
       }
     },
     'when data uses param that does not exist': {
       'then value should be the original data': function (topic) {
-        var val = topic.apply('hello ${foo} world', { blah: 'bar' });
-        assert.equal(val, 'hello ${foo} world');
+        var value = topic.apply('hello ${foo} world', { blah: 'bar' });
+        assert.equal(value, 'hello ${foo} world');
       }
     },
     'when data uses param that exists': {
       'then value should contain the param value': function (topic) {
-        var val = topic.apply('hello ${foo} world', { foo: 'bar' });
-        assert.equal(val, 'hello bar world');
+        var value = topic.apply('hello ${foo} world', { foo: 'bar' });
+        assert.equal(value, 'hello bar world');
       }
     },
     'when data contains multiple params that three exists one does not': {
       'then value should contain the params value': function (topic) {
-        var val = topic.apply('hello ${foo} world ${blah} hey ${foo} now ${a}', { foo: 'bar', a: 'b' });
-        assert.equal(val, 'hello bar world ${blah} hey bar now b');
+        var value = topic.apply('hello ${foo} world ${blah} hey ${foo} now ${a}', { foo: 'bar', a: 'b' });
+        assert.equal(value, 'hello bar world ${blah} hey bar now b');
       }
     },
     'when data contains now function call': {
       'then value should contain now value': function (topic) {
-        var val = topic.apply('hello ${now(\'yyyy\')} world', { foo: 'bar' });
-        assert.equal(val, 'hello 2000 world');
+        var value = topic.apply('hello ${now(\'yyyy\')} world', { foo: 'bar' });
+        assert.equal(value, 'hello 2000 world');
       }
     }
   },
@@ -58,26 +58,26 @@ vows.describe('util').addBatch({
     },
     'when dsv is null': {
       'then value should be undefined': function (topic) {
-        var val = topic.val(null, { foo: 'bar' });
-        assert.isUndefined(val);
+        var value = topic.val(null, { foo: 'bar' });
+        assert.isUndefined(value);
       }
     },
     'when object is null': {
       'then value should be undefined': function (topic) {
-        var val = topic.val('x.y.z', null);
-        assert.isUndefined(val);
+        var value = topic.val('x.y.z', null);
+        assert.isUndefined(value);
       }
     },
     'when property does not exist in object': {
       'then value should be undefined': function (topic) {
-        var val = topic.val('x.y.z', { foo: 'bar' });
-        assert.isUndefined(val);
+        var value = topic.val('x.y.z', { foo: 'bar' });
+        assert.isUndefined(value);
       }
     },
     'when property exists in object': {
       'then value should be undefined': function (topic) {
-        var val = topic.val('x.y.z', { x: { y: { z: 'bar' } } });
-        assert.equal(val, 'bar');
+        var value = topic.val('x.y.z', { x: { y: { z: 'bar' } } });
+        assert.equal(value, 'bar');
       }
     }
   }
