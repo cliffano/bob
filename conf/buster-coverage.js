ecdb97ae3553772a88afd1cb8d7e9edd3f122a68
Cliffano Subagio <cliffano@gmail.com>
2013-10-18 08:35:26 +1100

    Add lcov report to buster-istanbul coverage.

--- conf/buster-coverage.js
+++ conf/buster-coverage.js
@@ -11,6 +11,7 @@ module.exports.unit = {
     require('buster-istanbul')
   ],
   'buster-istanbul': {
-    outputDirectory: '.bob/report/coverage/buster-istanbul/'
+    outputDirectory: '.bob/report/coverage/buster-istanbul/',
+    formats: ['text', 'lcov']
   }
 };
