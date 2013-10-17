e1eb6255d89b4480be86635ea02bb8c88a4b4043
Cliffano Subagio <cliffano@gmail.com>
2013-10-17 19:46:07 +1100

    Add config.load tests.

---
+++ test/config.js
@@ -0,0 +1,44 @@
+var buster = require('buster'),
+  config = require('../lib/config'),
+  fs = require('fs');
+
+buster.testCase('testconfig - load', {
+  setUp: function () {
+    this.mockFs = this.mock(fs);
+  },
+  'should pass empty object when config file does not exist': function (done) {
+    this.mockFs.expects('exists').withArgs('somedir/.bob.json').callsArgWith(1, false);
+    config.load([], 'somedir', function (err, result) {
+      assert.isNull(err);
+      assert.equals(result, {});
+      done();
+    });
+  },
+  'should pass specified task in config object when config file exists': function (done) {
+    this.mockFs.expects('exists').withArgs('somedir/.bob.json').callsArgWith(1, true);
+    this.mockFs.expects('readFile').withArgs('somedir/.bob.json').callsArgWith(1, null, '{ "sometask": "foobar" }');
+    config.load(['sometask'], 'somedir', function (err, result) {
+      assert.isNull(err);
+      assert.equals(result, { sometask: 'foobar' });
+      done();
+    });
+  },
+  'should not pass specified task in config object when task does not exist': function (done) {
+    this.mockFs.expects('exists').withArgs('somedir/.bob.json').callsArgWith(1, true);
+    this.mockFs.expects('readFile').withArgs('somedir/.bob.json').callsArgWith(1, null, '{ "sometask": "foobar" }');
+    config.load(['sometask'], 'somedir', function (err, result) {
+      assert.isNull(err);
+      assert.equals(result, { sometask: 'foobar' });
+      done();
+    });
+  },
+  'should pass error when an error occurs while reading config file': function (done) {
+    this.mockFs.expects('exists').withArgs('somedir/.bob.json').callsArgWith(1, true);
+    this.mockFs.expects('readFile').withArgs('somedir/.bob.json').callsArgWith(1, new Error('someerror'));
+    config.load(['sometask'], 'somedir', function (err, result) {
+      assert.equals(err.message, 'someerror');
+      assert.equals(result, undefined);
+      done();
+    });
+  }
+});
