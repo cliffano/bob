e9ff0ef54f99e5e40e26a01ca16eb3a3c0782a73
Cliffano Subagio <cliffano@gmail.com>
2013-12-15 15:43:53 +1100

    Add deps unit test.

---
+++ test/deps.js
@@ -0,0 +1,43 @@
+var buster = require('buster'),
+  deps = require('../lib/deps'),
+  fs = require('fs'),
+  referee = require('referee'),
+  assert = referee.assert;
+
+buster.testCase('testdeps - install', {
+  setUp: function () {
+    this.mockConsole = this.mock(console);
+    this.mockFs = this.mock(fs);
+  },
+  'should pass error to callback when an error occurs while reading node_modules directory': function (done) {
+    this.mockFs.expects('readdir').once().withArgs('somedir/node_modules').callsArgWith(1, new Error('someerror'));
+    deps.install([ 'buster', 'kaiju', 'npm' ], { dir: 'somedir' }, function (err) {
+      assert.equals(err.message, 'someerror');
+      done();
+    });
+  },
+  'should only install uninstalled and non-global modules': function (done) {
+    this.mockConsole.expects('log').once().withExactArgs('[deps] Installing modules: %s (might take a while, once-off only)', 'kaiju');
+    var mockCanihaz = function (opts) {
+      assert.equals(opts.key, 'optDependencies');
+      return function (depNames, cb) {
+        assert.equals(depNames, 'kaiju');
+        cb();
+      };
+    }
+    var installed = ['buster'];
+    this.mockFs.expects('readdir').once().withArgs('somedir/node_modules').callsArgWith(1, null, installed);
+    deps.install([ 'buster', 'kaiju', 'npm' ], { canihaz: mockCanihaz, dir: 'somedir' }, function (err) {
+      assert.equals(err, undefined);
+      done();
+    });    
+  },
+  'should not do anything when all modules are already installed': function (done) {
+    var installed = ['buster', 'kaiju'];
+    this.mockFs.expects('readdir').once().withArgs('somedir/node_modules').callsArgWith(1, null, installed);
+    deps.install([ 'buster', 'kaiju', 'npm' ], { dir: 'somedir' }, function (err) {
+      assert.equals(err, undefined);
+      done();
+    });
+  }
+});
